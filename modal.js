3dc19cce683d4f6f58d38a7874ccbf495b06f4b8
Vladimir Poddukin <poddukinvladimir@gmail.com>
2017-07-28 15:00:51 +0300

    first commit

---
+++ modal.js
@@ -0,0 +1,16 @@
+$(document).ready($(function() {
+    // modal window animation
+    $('#showModalButton').click(function() {
+        $('#modal-window').fadeToggle();
+
+    });
+    $(document).mouseup(function (e) {
+        let container = $("#modal-window");
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            container.fadeOut();
+        }
+    });
+
+}));
